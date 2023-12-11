0b5abe9607f1a56ab6597545bb6c16687020cb55
hyenaManager <zodiacjelly9@gmail.com>
2023-12-11 10:39:03 +0630

    moodle setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 /* eslint-disable no-unused-vars */
-import { Routes, Route } from 'react-router-dom'
-import ErrorPage from './pages/ErrorPage'
-import React from 'react'
-import SplashPage from './pages/SplashPage'
-import Signup from './pages/Signup'
-import Login from './pages/Login'
-import Home from './pages/Home'
+import { Routes, Route } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage";
+import React from "react";
+import SplashPage from "./pages/SplashPage";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import AddNewPage from "./pages/addNewPage";
 
 const App = () => (
   <Routes>
-    <Route path="/" element={<SplashPage/>}></Route>
-    <Route path='/signup' element={<Signup/>}></Route>
-    <Route path='/login' element={<Login/>}></Route>
-    <Route path='/home' element={<Home/>}></Route>
-    <Route path="*" element={<ErrorPage/>}> </Route>
+    <Route path="/" element={<SplashPage />}></Route>
+    <Route path="/signup" element={<Signup />}></Route>
+    <Route path="/login" element={<Login />}></Route>
+    <Route path="/home" element={<Home />}></Route>
+    <Route path="*" element={<ErrorPage />}></Route>
+    <Route path="/preorderPage/addNewPage" element={<AddNewPage />}></Route>
   </Routes>
-)
+);
 
-export default App
+export default App;
---
+++ src/components/Navbar/moodles/saleModle.jsx
@@ -0,0 +1,42 @@
+export default function SaleMoodle() {
+  return (
+    <>
+      <div className=" fixed w-full h-full top-0 left-0 flex justify-center items-center">
+        <section className=" relative flex flex-col justify-start p-5 border-2">
+          <span>Back button</span>
+          <h3>Town</h3>
+          <input />
+          <h4>Order items</h4>
+          <hr />
+          <ul>
+            <li className=" flex justify-between p-1">
+              <h5>blah blah</h5>
+              <h5>10 bottle</h5>
+              <h5>1000ks</h5>
+            </li>
+            <li className=" flex justify-between p-1">
+              <h5>blah blah</h5>
+              <h5>10 bottle</h5>
+              <h5>1000ks</h5>
+            </li>
+            <li className=" flex justify-between p-1">
+              <h5>blah blah</h5>
+              <h5>10 bottle</h5>
+              <h5>1000ks</h5>
+            </li>
+            <li className=" flex justify-between p-1">
+              <h5>blah blah</h5>
+              <h5>10 bottle</h5>
+              <h5>1000ks</h5>
+            </li>
+          </ul>
+          <hr />
+          <div className=" flex justify-start">
+            <h5>Total : </h5>
+            <span>1000 ks</span>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+}
