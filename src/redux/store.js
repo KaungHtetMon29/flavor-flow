209f998b0775d1b9a726b197c837db438c3d626c
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-12-12 14:27:10 +0630

    Implement client slice reducer and functions

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,11 +3,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import authSliceReducer from "./authSlice";
 import preOrderSliceReducer from "./preOrderSlice";
 import deliverySliceReducer from "./deliverySlice";
+import clientSliceReducer from "./clientSlice";
 const store = configureStore({
     reducer: {
         authentication: authSliceReducer,
         preOrder: preOrderSliceReducer,
         delivery: deliverySliceReducer,
+        client: clientSliceReducer,
     }
 })
 
