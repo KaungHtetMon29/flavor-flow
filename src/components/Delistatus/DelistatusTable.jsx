85a1d7970dfaf823fac58ce0da72e76985d0d881
KaungHtetMon29 <kaunghtetmon456@gmail.com>
2023-12-13 14:09:17 +0630

    delimoodle

--- src/components/Delistatus/DelistatusTable.jsx
+++ src/components/Delistatus/DelistatusTable.jsx
@@ -18,109 +18,95 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
 import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
-import { SkeletonDemo } from "../SkeletonFile/SkeletonFile";
+import TruckMoodle from "../moodles/truckMoodle";
+import { IoCar } from "react-icons/io5";
+import DeliveryMoodle from "../moodles/deliveryModle";
 
 const DelistatusTable = () => {
-  const [status, setStatus] = React.useState("Sending");
+  const [status, setStatus] = React.useState("sending");
   const [isArrowUp, setIsArrowUp] = useState(false);
+  const [showDetail, setShowDetail] = useState(false);
   const handleDropdownOpenChange = (isOpen) => {
     setIsArrowUp(isOpen);
   };
-  const handlerClick = () => {
-    //click functionality
-  };
-  const data = [
-    { id: 1, name: "John Doe", truckId: "012", region: "Yangon" },
-    { id: 2, name: "John Doe", truckId: "02", region: "Yangon" },
-    { id: 3, name: "John Doe", truckId: "12", region: "Yangon" },
-    { id: 4, name: "John Doe", truckId: "02", region: "Yangon" },
-  ];
-
+  // const handlerClick = () => {
+  //   //click functionality
+  // };
   return (
-    <Table>
-      <TableHeader className="sticky top-0 bg-white">
-        <TableRow>
-          <TableHead className="w-[200px]">Preorder ID</TableHead>
-          <TableHead>Truck ID</TableHead>
-          <TableHead>Region</TableHead>
-          <TableHead>Status</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {data?.length === 0 ? ( // change in conditional
-          <>
-            <TableCell>
-              <SkeletonDemo />
-            </TableCell>
-            <TableCell>
-              <SkeletonDemo />
-            </TableCell>
-            <TableCell>
-              <SkeletonDemo />
-            </TableCell>
-            <TableCell>
-              <SkeletonDemo />
-            </TableCell>
-          </>
-        ) : (
-          <>
-            {data.map((d) => (
-              <TableRow key={d.id}>
-                <TableCell className="font-medium">{d.id}</TableCell>
-                <TableCell>{d.truckId}</TableCell>
-                <TableCell>{d.region}</TableCell>
-                <TableCell>
-                  {/* //dropdown// */}
-                  <DropdownMenu onOpenChange={handleDropdownOpenChange}>
-                    <DropdownMenuTrigger asChild>
-                      <Button
-                        variant="outline"
-                        className="flex gap-3 justify-start w-40"
-                      >
-                        <div>
-                          <p className="text-start text-[18px] font-normal">
-                            {status}
-                          </p>
-                        </div>
-                        <div className="justify-end flex w-full">
-                          {isArrowUp ? <IoIosArrowUp /> : <IoIosArrowDown />}
-                        </div>
-                      </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent className="w-56">
-                      <DropdownMenuSeparator />
-                      <DropdownMenuRadioGroup
-                        value={status}
-                        onValueChange={setStatus}
-                      >
-                        <DropdownMenuRadioItem value="Sending">
-                          Sending
-                        </DropdownMenuRadioItem>
-                        <DropdownMenuRadioItem value="Pending">
-                          Pending
-                        </DropdownMenuRadioItem>
-                        <DropdownMenuRadioItem value="Cancel">
-                          Cancel
-                        </DropdownMenuRadioItem>
-                      </DropdownMenuRadioGroup>
-                    </DropdownMenuContent>
-                  </DropdownMenu>
-                </TableCell>
-                <TableCell>
+    <>
+      {showDetail && <DeliveryMoodle hide={() => setShowDetail(false)} />}
+      <Table>
+        <TableHeader className="sticky top-0 bg-white">
+          <TableRow>
+            <TableHead className="w-[200px] text-[22px]">Preorder ID</TableHead>
+            <TableHead className="text-[22px]">Truck ID</TableHead>
+            <TableHead className="text-[22px]">Region</TableHead>
+            <TableHead className="text-[22px]">Status</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow onClick={() => setShowDetail(true)}>
+            <TableCell className="font-medium">1</TableCell>
+            <TableCell className="text-[18px]">12/321</TableCell>
+            <TableCell className="text-[18px]">Yangon</TableCell>
+            <TableCell
+              onClick={(e) => {
+                e.stopPropagation();
+              }}
+            >
+              {/* //dropdown// */}
+              <DropdownMenu onOpenChange={handleDropdownOpenChange}>
+                <DropdownMenuTrigger asChild>
                   <Button
-                    variant={"outline"}
-                    onClick={handlerClick}
-                    className="text-[18px]"
+                    variant="outline"
+                    className="flex gap-3 w-32 justify-start"
                   >
-                    Assignment
+                    <div className="">
+                      <p className="text-start">{status}</p>
+                    </div>
+                    <div className="justify-end flex w-full">
+                      {isArrowUp ? <IoIosArrowUp /> : <IoIosArrowDown />}
+                    </div>
                   </Button>
-                </TableCell>
-              </TableRow>
-            ))}
-          </>
-        )}
-      </TableBody>
-    </Table>
+                  {/* <Button variant="outline" className="flex gap-3">
+                  {status}
+                  {isArrowUp ? <IoIosArrowUp /> : <IoIosArrowDown />}
+                </Button> */}
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-56">
+                  <DropdownMenuLabel>Order Status</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuRadioGroup
+                    value={status}
+                    onValueChange={setStatus}
+                  >
+                    <DropdownMenuRadioItem value="sending">
+                      Sending
+                    </DropdownMenuRadioItem>
+                    <DropdownMenuRadioItem value="pending">
+                      Pending
+                    </DropdownMenuRadioItem>
+                    <DropdownMenuRadioItem value="cancel">
+                      Cancel
+                    </DropdownMenuRadioItem>
+                  </DropdownMenuRadioGroup>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </TableCell>
+            <TableCell
+              onClick={(e) => {
+                e.stopPropagation();
+              }}
+            >
+              {/* Truck Moodle */}
+              <TruckMoodle>
+                <IoCar />
+              </TruckMoodle>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </>
   );
 };
 
